c3a3444e67ac22b9ff6463d2b4a98681a790e97a
Zwe Thuta <hermez27zz@gmail.com>
2024-11-22 10:30:38 +0630

    Form Updated

--- src/addBook.php
+++ src/addBook.php
@@ -4,8 +4,32 @@ try {
     $sql = "select * from category";
     $stmt = $conn->query($sql);
     $categories =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql = "select * from publisher";
+    $stmt = $conn->query($sql);
+    $publishers =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql = "select * from author";
+    $stmt = $conn->query($sql);
+    $authors =  $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo "" . $e->getMessage() . "";
+}
+
+if (isset($_POST["insert"])) {
+    $title = $_POST['title'];
+    $categories = $_POST['categories'];
+    $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
+    $author = $_POST['author'];
+    $publisher = $_POST['publisher'];
+    $year = $_POST['year'];
+
+    $file_name = $_FILES['cover']['name'];
+    // $tempname = $_FILES['cover']['tmp_name'];
+    $folder = "../covers/". $file_name;
+    move_uploaded_file($_FILES['cover']['tmp_name'], $folder);
+
 }
 ?>
 <!doctype html>
@@ -55,28 +79,20 @@ try {
     </nav>
     <br>
 
-    <div class="flex justify-center">
-        <div class="w-30">
-            <img src="../image/cat.png" alt="">
-        </div>
+    <div class="flex justify-center ">
+    <div class="image-container my-8" style="width: 80;"> 
+    <!-- <img src="../image/books.gif" alt="Books" loading="lazy" class="responsive-image"> -->
+</div>
         <div class="mt-20 px-28">
-            <form class=" mx-auto px-24">
+            <form class=" mx-auto px-24" method="POST" enctype="multipart/form-data" action="<?php $_SERVER['PHP_SELF'] ?>">
                 <p class="text-4xl font-medium text-gray-900 dark:text-black">Add a new book</p><br>
                 <div class="relative z-0 w-full mb-5 group">
                     <input type="text" name="title" id="title" class="block py-2.5 px-36 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                     <label for="title" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
-                </div>
+                </div><br>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="number" name="price" id="price" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required min="0" />
-                    <label for="price" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <input type="number" name="quantity" id="quantity" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required min="1" />
-                    <label for="quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <label for="categories" class="block mb-2 text-sm font-medium text-gray-200 dark:text-white">Select category</label>
-                    <select id="categories" class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-300 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select id="categories" name="categories" class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-300 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="" disabled selected>Select a category</option>
                         <?php if (isset($categories)) {
                             foreach ($categories as $category) {
                                 echo "<option value=$category[category_id]>$category[category_name]</option>";
@@ -84,9 +100,47 @@ try {
                         }
                         ?>
                     </select>
-
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="number" name="price" id="price" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required min="0" />
+                    <label for="price" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="number" name="quantity" id="quantity" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required min="1" />
+                    <label for="quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label><br>
+                </div>
+
+                <div class="relative z-0 w-full mb-5 group">
+                    <select id="publisher" name="publisher" class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-300 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="" disabled selected>Select a publisher</option>
+                        <?php if (isset($publishers)) {
+                            foreach ($publishers as $publisher) {
+                                echo "<option value=$publisher[publisher_id]>$publisher[publisher_name]</option>";
+                            }
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <select id="author" name="author" class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-300 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="" disabled selected>Select a author</option>
+                        <?php if (isset($authors)) {
+                            foreach ($authors as $author) {
+                                echo "<option value=$author[author_id]>$author[author_name]</option>";
+                            }
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="number" name="year" id="year" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                    <label for="year" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Year</label>
+                </div><br>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="file" name="cover" id="cover" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
+                    <label for="cover" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Book Cover</label>
+                </div>
+                <button type="submit" name="insert" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
         </div>
 </body><br>
